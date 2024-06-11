3f57518b748d2c184aa98a71010943974a822737
sheymantech <olaniyansheyi1704@gmail.com>
2024-06-11 04:48:07 +0100

    new feature

--- src/views/PageNotFoundView.vue
+++ src/views/PageNotFoundView.vue
@@ -1,5 +1,17 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
-  <div>PAGE-NOT-FOUND VIEW</div>
+  <div class="flex justify-center items-center text-center flex-col px-5 text-white my-16">
+    <h3 class="font-semibold text-2xl tracking-wide">
+      Page not found,please go back to the home page.
+    </h3>
+    <RouterLink
+      to="/"
+      class="px-6 py-3 rounded-lg text-primary font-semibold bg-accent hover:bg-accent-hover mt-8"
+    >
+      Go Back Home
+    </RouterLink>
+  </div>
 </template>
